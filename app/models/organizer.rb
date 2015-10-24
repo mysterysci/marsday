8fc2974b2bf9d1afb8e8e6e01db69bd4f1dd15a2
Jared Seehafer <jared@seehafer.net>
2015-10-24 16:59:20 -0700

    organizer form working

--- app/models/organizer.rb
+++ app/models/organizer.rb
@@ -3,9 +3,6 @@ class Organizer < ActiveRecord::Base
   validates :name,  presence: true, uniqueness:true
   validates :email,  presence: true, uniqueness:true
   validates :address,  presence: true
-  validates :city,  presence: true
-  validates :state, presence: true
-  validates :zipcode,  presence: true
   validates :num_scopes,  presence: true
   validates :max_attendees,  presence: true
 
