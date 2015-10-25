480f6d283b5b2617ff87f098b64b780f8dc54e52
David Crawford <david.crawford@gmail.com>
2015-10-25 11:28:27 -0700

    Revert

--- config/application.rb
+++ config/application.rb
@@ -30,14 +30,6 @@ class ActiveRecordOverrideRailtie < Rails::Railtie
   end
 end
 
-RGeo::ActiveRecord::SpatialFactoryStore.instance.tap do |config|
-  # By default, use the GEOS implementation for spatial columns.
-  config.default = RGeo::Geos.factory_generator
-
-  # But use a geographic implementation for point columns.
-  config.register(RGeo::Geographic.spherical_factory(srid: 4326), geo_type: "point")
-end
-
 module Saturnday
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
