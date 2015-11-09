6c4bc8414600bd2bd576f8bd979df0d30c80e3a9
Keith Schacht <krschacht@gmail.com>
2015-11-08 21:06:04 -0800

    rename app to marsday

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ class ActiveRecordOverrideRailtie < Rails::Railtie
   end
 end
 
-module Saturnday
+module Marsday
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
